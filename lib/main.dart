f59caf00a47b169a292bd16d5ae88ae777f6fa64
zdravkostanin1 <zdravko.stanin@gmail.com>
2025-01-03 15:11:10 +0200

    more code tidy.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:personal_website/src/app.dart';
-import 'package:personal_website/src/pages/projects_page/view/projects_page.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => LayoutBuilder(
-              builder: (context, constraints) {
-                if (constraints.maxWidth < 600) {
-                  // mobile view
-                  return const CardView();
-                } else if (constraints.maxWidth < 1200) {
-                  // tablet view
-                  return const WebView();
-                } else {
-                  // desktop view
-                  return const WebView();
-                }
-              },
-            ),
-        '/projects': (context) => const ProjectsPage(),
-      },
-    ),
-  );
+  runApp(const App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,3 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:personal_website/src/pages/card_view/view/card_view.dart';
+import 'package:personal_website/src/pages/projects_page/view/projects_page.dart';
+import 'package:personal_website/src/pages/web_view/view/web_view.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => LayoutBuilder(
+              builder: (context, constraints) {
+                if (constraints.maxWidth < 600) {
+                  // mobile view
+                  return const CardView();
+                } else if (constraints.maxWidth < 1200) {
+                  // tablet view
+                  return const WebView();
+                } else {
+                  // desktop view
+                  return const WebView();
+                }
+              },
+            ),
+        '/projects': (context) => const ProjectsPage(),
+      },
+    );
+  }
+}
